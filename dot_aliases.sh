41a8105f01dd75184cf6665905f2ae0f4dc72796
Tumi Oguntola <tumi.oguntola@gmail.com>
2026-06-21 22:52:42 -0600

    Added & cleaned-up aliases

--- dot_aliases.sh
+++ dot_aliases.sh
@@ -1,19 +1,27 @@
-## Colorize the ls output
+## Listing
 alias ls='ls --color=auto'
-
-## Use a long listing format
 alias ll='ls -la'
+alias l.='ls --color=auto -d .*'
+alias sizeup='du -ha -d 1 | sort -hr'
 
-## Show hidden files
-alias l.='ls --color=auto -d .* '
-
-## Git aliases
+## Git
 alias g='git'
 alias gs='git status'
 alias gc='git commit -m'
 
-## Alias for fastfetch
-alias ff='fastfetch --config ~/.config/fastfetch/config.jsonc'
+## Kubernetes
+alias kube='kubectl'
+alias ranch='rancher'
 
-## Relaod the shell configuration
+## Tailscale
+alias tailscale='/Applications/Tailscale.app/Contents/MacOS/Tailscale'
+alias ts='tailscale'
+
+## Misc
+alias ff='fastfetch --config "$HOME/.config/fastfetch/config.jsonc"'
 alias reload='exec zsh'
+alias please='sudo'
+
+## Directories
+github="$HOME/Documents/GitHub"
+vault="$HOME/Documents/Library of Alexandria"
